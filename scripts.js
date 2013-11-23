86d272118128fdc95a79ac19976acb2775055a9f
Hack Reactor Students <kasikamla@gmail.com>
2013-11-23 12:42:56 -0800

    draggable but laggy

--- scripts.js
+++ scripts.js
@@ -1,38 +1,82 @@
 
+$(document).ready(function(){
+  addPlayer();
+  add100Enemies();
+  $('body').on("mousedown",".playerCircle",function(){
+    $(document).mousemove(function(e){
+      movePlayer(e);
+    });
+  });
+  setInterval(moveEnemies,2000);
+});
+
 var gameWidth = 740;
 var gameHeight = 585;
 
 var svg = d3.select("body").append("svg").attr("width",gameWidth).attr("height",gameHeight);
 
+//PLAYER FUNCTIONS
+
+var addPlayer= function() {
+  var player = svg.selectAll(".playerCircle").data([{cx: gameWidth/2, cy: gameHeight/2}]);
+
+  var addPlayer = player.enter().append("circle").attr("class", "playerCircle");
+
+  addPlayer.attr("cy", function(d) {
+    return d.cy;
+  });
+
+  addPlayer.attr("cx", function(d) {
+    return d.cx;
+  });
+
+  addPlayer.attr("r", "10").attr("fill","orange");
+};
+
+var movePlayer = function(mousePos) {
+  //get mouse position and update data element with new position
+  var player = svg.selectAll(".playerCircle").data([{cx: mousePos.pageX, cy: mousePos.pageY}]);
+  player.transition().duration(300).attr("cy", function(d) {
+   return d.cy;
+  }).attr("cx", function(d) {
+   return d.cx;
+  });
+
+  //Handle if mouse is outside of bounds??
+};
+
+
+
+// ENEMY FUNCTIONS
 //Generate an array of 100 objects with a randomnly chosen cx and cy propoerties
-var getPos = function(){
+
+var getEnemyPos = function(){
   return _.map(_.range(100),function(){
     return {cx:Math.random()*(gameWidth-10), cy:Math.random()*(gameHeight-10)};
   });
 };
 
-
 var add100Enemies = function(){
   //selects the circles and attaches data (enemyPos) to it
-  var enemies= svg.selectAll("circle").data(getPos());
+  var enemies= svg.selectAll(".enemyCircle").data(getEnemyPos());
 
   // add a circle for each enemy position
-  var addCircles = enemies.enter().append("circle");
+  var addEnemies = enemies.enter().append("circle").attr("class", "enemyCircle");
 
   // give each circle a position based on the data, and a radius of 10
-  addCircles.attr("cy", function(d) {
+  addEnemies.attr("cy", function(d) {
    return d.cy;
   });
 
-  addCircles.attr("cx", function(d) {
+  addEnemies.attr("cx", function(d) {
    return d.cx;
   });
 
-  addCircles.attr("r","10");
+  addEnemies.attr("r","10");
 };
 
-var enemiesMove = function(){
-  var enemies= svg.selectAll("circle").data(getPos());
+var moveEnemies = function(){
+  var enemies= svg.selectAll(".enemyCircle").data(getEnemyPos());
   enemies.transition().duration(2000).attr("cy", function(d) {
    return d.cy;
   }).attr("cx", function(d) {
@@ -40,8 +84,10 @@ var enemiesMove = function(){
   });
 };
 
-add100Enemies();
-setInterval(enemiesMove,2000);
+
+
+
+
 
 // function() {
 //  svg.append("enemy")
